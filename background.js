991218010ead61a7bc0a42cf692ea7196202e9f2
mencina <me@klooff.com>
2014-02-18 18:09:59 -0500

    added messaging functionality

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+// browser action clicked
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create ({url: 'http://www.reddit.com'}, function () {console.log("opened")})
+	// query for active tabs on this window
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		// callback from query, send message to active tab
+		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+			// callback from response, print
+			console.log(response.farewell);
+		});
+	});
 }); 
--- paladin.js
+++ paladin.js
@@ -1,2 +1,14 @@
-$(document).ready(function () {
-});
+chrome.runtime.onMessage.addListener(
+	// message listener
+	function(request, sender, sendResponse) {
+		// see if message was sent from content script running on a tab or an extension
+		console.log(sender.tab ?
+			"from a content script:" + sender.tab.url :
+			"from the extension");
+
+		// see if the message was sent from desired extension
+		if (request.greeting == "hello")
+			// call sendResponse
+			sendResponse({farewell: "goodbye"});
+	}
+);
