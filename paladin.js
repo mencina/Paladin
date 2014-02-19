ab537dafe15fbff74d8117847f9198275ee81631
mencina <me@klooff.com>
2014-02-18 20:20:38 -0500

    retrieve relevant info functionality

--- paladin.js
+++ paladin.js
@@ -6,6 +6,19 @@ chrome.runtime.onMessage.addListener(
 			"from a content script:" + sender.tab.url :
 			"from the extension");
 
+		$('#search-results').children().each( function () {
+
+			video_title = $(this).find("a .yt-ui-ellipsis-wrapper").text()
+			author = $(this).find(".yt-user-name")
+			author_url = author.attr("href")
+			author_name = author.text()
+
+			console.log ('Video Title: ' + video_title)
+			console.log ('Video URL: ' + author_url)
+			console.log ('Author Name: ' + author_name)
+
+		});
+
 		// see if the message was sent from desired extension
 		if (request.greeting == "hello")
 			// call sendResponse
