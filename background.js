6f374db424251b63eaf437fc52976e6b999bfc7b
mencina <me@klooff.com>
2014-02-18 21:27:30 -0500

    send array of objects to background

--- background.js
+++ background.js
@@ -3,9 +3,13 @@ chrome.browserAction.onClicked.addListener(function() {
 	// query for active tabs on this window
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		// callback from query, send message to active tab
-		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+		chrome.tabs.sendMessage(tabs[0].id, "init", function(response) {
 			// callback from response, print
-			console.log(response.farewell);
+			console.log(response);
+
+			var url = "http://www.youtube.com" + response[0].author_url
+			chrome.tabs.create({"url": url})
+
 		});
 	});
 }); 
--- paladin.js
+++ paladin.js
@@ -5,23 +5,29 @@ chrome.runtime.onMessage.addListener(
 		console.log(sender.tab ?
 			"from a content script:" + sender.tab.url :
 			"from the extension");
+		var response = []
+		
+		if (request == "init") {
+			$('#search-results').children().each( function () {
 
-		$('#search-results').children().each( function () {
+				video_title = $(this).find("a .yt-ui-ellipsis-wrapper").text()
+				author = $(this).find(".yt-user-name")
+				author_url = author.attr("href")
+				author_name = author.text()
 
-			video_title = $(this).find("a .yt-ui-ellipsis-wrapper").text()
-			author = $(this).find(".yt-user-name")
-			author_url = author.attr("href")
-			author_name = author.text()
+				console.log ('Video Title: ' + video_title)
+				console.log ('Author URL: ' + author_url)
+				console.log ('Author Name: ' + author_name)
 
-			console.log ('Video Title: ' + video_title)
-			console.log ('Video URL: ' + author_url)
-			console.log ('Author Name: ' + author_name)
+				var data = {
+					'video_title': video_title,
+					'author_url': author_url,
+					'author_name': author_name
+				};
 
-		});
-
-		// see if the message was sent from desired extension
-		if (request.greeting == "hello")
-			// call sendResponse
-			sendResponse({farewell: "goodbye"});
+				response.push(data)
+			});
+		}
+		sendResponse(response);
 	}
 );
